9dad809ee457d9a40c05e73da6e4958e58852636
CK <choonkiat.lee@gmail.com>
2020-04-02 02:36:49 +0800

    update

--- model/wdsr.py
+++ model/wdsr.py
@@ -16,7 +16,7 @@ def wdsr_b(scale, num_filters=32, num_res_blocks=8, res_block_expansion=6, res_b
     print("Created wdsr_b model")
     return wdsr(scale, num_filters, num_res_blocks, res_block_expansion, res_block_scaling, res_block_b)
 
-class NormalizeLayer(tensorflow.keras.layers.Layer):
+class NormalizeLayer(tf.keras.layers.Layer):
     def __init__(self):
         self.rgb_mean = tf.constant([0.4488, 0.4371, 0.4040]) * 255
         super(NormalizeLayer, self).__init__()
@@ -25,7 +25,7 @@ class NormalizeLayer(tensorflow.keras.layers.Layer):
         return (inputs - rgb_mean) / 127.5
 
     def get_config(self):
-        return {'units': self.units}
+        return {'rgb_mean': self.rgb_mean}
 
 
 
