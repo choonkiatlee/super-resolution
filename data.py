a7fa907bc129bc0b8bc9c38631f9c7e2202915cc
CK <choonkiat.lee@gmail.com>
2020-04-02 23:25:19 +0800

    update

--- data.py
+++ data.py
@@ -61,6 +61,7 @@ class DIV2K:
             ds = ds.map(lambda lr, hr: random_crop(lr, hr, scale=self.scale), num_parallel_calls=AUTOTUNE)
             ds = ds.map(random_rotate, num_parallel_calls=AUTOTUNE)
             ds = ds.map(random_flip, num_parallel_calls=AUTOTUNE)
+        ds.apply(tf.contrib.data.unbatch())
         ds = ds.batch(batch_size)
         ds = ds.repeat(repeat_count)
         ds = ds.prefetch(buffer_size=AUTOTUNE)
