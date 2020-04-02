2ad9df9b465c235f8f306ff05d648d9e3917e7ea
CK <choonkiat.lee@gmail.com>
2020-04-03 02:59:28 +0800

    update

--- convert_to_tf_lite.py
+++ convert_to_tf_lite.py
@@ -1,13 +1,14 @@
 import os
 import tensorflow as tf
 
+def psnr(y_true, y_pred):
+    return tf.image.psnr(y_true, y_pred, max_val=255)
 
 if os.path.exists('saved_model'):
     print("Loaded previously saved model")
     our_model = tf.keras.models.load_model('saved_model', custom_objects={'psnr': psnr})
 
-def psnr(y_true, y_pred):
-    return tf.image.psnr(y_true, y_pred, max_val=255)
+
 
 converter = tf.lite.TFLiteConverter.from_keras_model(our_model)
 
